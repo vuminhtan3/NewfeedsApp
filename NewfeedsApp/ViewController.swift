9129c906dc4d5a95a2fa5491b9a874fcc24298da
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-06-02 19:29:43 +0700

    init project

---
+++ NewfeedsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewfeedsApp
+//
+//  Created by Minh Tan Vu on 02/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
